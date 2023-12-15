c5580179aad0ad841531fad28662ceef3029da75
Kubilay Birer <kubilaybirer@hotmail.com>
2023-12-15 15:13:33 +0300

    added graph and node classes, worked on colouring algorithm

--- YazGelLab1/Form1.cs
+++ YazGelLab1/Form1.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+using System.Xml.Linq;
+using static YazGelLab1.Form1;
 
 namespace YazGelLab1
 {
@@ -29,47 +32,151 @@ namespace YazGelLab1
         {
 
         }
-        private void RenklendirmeAlgoritmasi(List<Ders> dersler)
+        //private void RenklendirmeAlgoritmasi(List<Ders> dersler)
+        //{
+        //    // Çakışma var mı kontrol etmek için iki dersin çakışıp çakışmadığını kontrol eden bir metot
+        //    bool CakismaVarMi(Ders ders1, Ders ders2)
+        //    {
+        //        if (ders1.DersinSaati.SaatDegeri == ders2.DersinSaati.SaatDegeri &&
+        //            ders1.DersinSinifi.SinifAdi == ders2.DersinSinifi.SinifAdi &&
+        //            ders1.DersinOgretmeni.Ad == ders2.DersinOgretmeni.Ad)
+        //        {
+        //            return true; // Çakışma var
+        //        }
+        //        return false; // Çakışma yok
+        //    }
+
+        //    // Çakışma kontrolü
+        //    for (int i = 0; i < dersler.Count; i++)
+        //    {
+        //        for (int j = i + 1; j < dersler.Count; j++)
+        //        {
+        //            if (CakismaVarMi(dersler[i], dersler[j]))
+        //            {
+        //                // Çakışma var, label2'yi güncelle
+        //                label3.Text = "Çakışma var";
+        //                return; // Çakışma olduğu için işlemi sonlandır
+        //            }
+        //        }
+        //    }
+
+        //    // Eğer hiç çakışma yoksa label2'yi güncelle
+        //    label3.Text = "Çakışma yok";
+        //}
+
+        // Node (Düğüm) sınıfı
+        public class Node
         {
-            // Çakışma var mı kontrol etmek için iki dersin çakışıp çakışmadığını kontrol eden bir metot
-            bool CakismaVarMi(Ders ders1, Ders ders2)
+            public string DersAdi { get; } // Dersin adı
+            public string Ogretmen { get; } // Öğretmen adı ya da kimliği
+            public string Sinif { get; } // Sınıf adı ya da kimliği
+            public string Saat { get; } // Dersin saati
+            public List<Edge> Neighbors { get; } // Komşu kenarlar
+
+            public Node(string dersAdi, string ogretmen, string sinif, string saat)
             {
-                if (ders1.DersinSaati.SaatDegeri == ders2.DersinSaati.SaatDegeri &&
-                    ders1.DersinSinifi.SinifAdi == ders2.DersinSinifi.SinifAdi &&
-                    ders1.DersinOgretmeni.Ad == ders2.DersinOgretmeni.Ad)
-                {
-                    return true; // Çakışma var
-                }
-                return false; // Çakışma yok
+                DersAdi = dersAdi;
+                Ogretmen = ogretmen;
+                Sinif = sinif;
+                Saat = saat;
+                Neighbors = new List<Edge>();
             }
 
-            // Çakışma kontrolü
-            for (int i = 0; i < dersler.Count; i++)
+            public void AddNeighbor(Edge edge)
             {
-                for (int j = i + 1; j < dersler.Count; j++)
-                {
-                    if (CakismaVarMi(dersler[i], dersler[j]))
-                    {
-                        // Çakışma var, label2'yi güncelle
-                        label3.Text = "Çakışma var";
-                        return; // Çakışma olduğu için işlemi sonlandır
-                    }
-                }
+                Neighbors.Add(edge);
             }
-
-            // Eğer hiç çakışma yoksa label2'yi güncelle
-            label3.Text = "Çakışma yok";
         }
+
+
+        // Edge (Kenar) sınıfı
+        public class Edge
+        {
+            public Node Source { get; } // Kenarın başlangıç düğümü
+            public Node Target { get; } // Kenarın bitiş düğümü
+
+            public Edge(Node source, Node target)
+            {
+                Source = source;
+                Target = target;
+            }
+        }
+
+        // Graph (Çizge) sınıfı
+        public class Graph
+        {
+            public List<Node> Nodes { get; } // Çizgedeki düğümler
+
+            public Graph()
+            {
+                Nodes = new List<Node>();
+            }
+
+            public void AddNode(Node node)
+            {
+                Nodes.Add(node);
+            }
+
+            public void AddEdge(Node source, Node target)
+            {
+                Edge edge = new Edge(source, target);
+                source.AddNeighbor(edge);
+                target.AddNeighbor(edge);
+            }
+        }
+
+        // Çakışma kontrolü için algoritma
+        void CheckConflicts(Graph graph)
+        {
+            bool conflict = false;
+
+            foreach (Node node in graph.Nodes)
+            {
+                int dersAdiCount = 0;
+                int ogretmenCount = 0;
+                int sinifCount = 0;
+                int saatCount = 0;
+
+                foreach (Edge edge in node.Neighbors)
+                {
+                    Node neighborNode = edge.Source == node ? edge.Target : edge.Source;
+
+                    if (node.DersAdi == neighborNode.DersAdi)
+                        dersAdiCount++;
+
+                    if (node.Ogretmen == neighborNode.Ogretmen)
+                        ogretmenCount++;
+
+                    if (node.Sinif == neighborNode.Sinif)
+                        sinifCount++;
+
+                    if (node.Saat == neighborNode.Saat)
+                        saatCount++;
+                }
+
+                if (dersAdiCount > 0 || ogretmenCount > 1 || sinifCount > 1 || saatCount > 1)
+                {
+                    conflict = true;
+                    break;
+                }
+            }
+
+            // Çakışma durumuna göre label3'ü güncelle
+            label3.Text = conflict ? "Çakışma var" : "Çakışma yok";
+        }
+
+
+
 
 
         public class Ders
         {
             public string DersAdi { get; set; }
-            public Ogretmen DersinOgretmeni { get; set; }
-            public Sinif DersinSinifi { get; set; }
-            public Saat DersinSaati { get; set; }
+            public string DersinOgretmeni { get; set; }
+            public string DersinSinifi { get; set; }
+            public string DersinSaati { get; set; }
 
-            public Ders(string dersAdi, Ogretmen dersinOgretmeni, Sinif dersinSinifi, Saat dersinSaati)
+            public Ders(string dersAdi, string dersinOgretmeni, string dersinSinifi, string dersinSaati)
             {
                 DersAdi = dersAdi;
                 DersinOgretmeni = dersinOgretmeni;
@@ -78,36 +185,36 @@ namespace YazGelLab1
             }
         }
 
-        public class Saat
-        {
-            public string SaatDegeri { get; set; }
+        //public class Saat
+        //{
+        //    public string SaatDegeri { get; set; }
 
-            public Saat(string saatDegeri)
-            {
-                SaatDegeri = saatDegeri;
-            }
-        }
+        //    public Saat(string saatDegeri)
+        //    {
+        //        SaatDegeri = saatDegeri;
+        //    }
+        //}
 
-        public class Sinif
-        {
-            public string SinifAdi { get; set; }
+        //public class Sinif
+        //{
+        //    public string SinifAdi { get; set; }
 
-            public Sinif(string sinifAdi)
-            {
-                SinifAdi = sinifAdi;
-            }
-        }
+        //    public Sinif(string sinifAdi)
+        //    {
+        //        SinifAdi = sinifAdi;
+        //    }
+        //}
 
 
-        public class Ogretmen
-        {
-            public string Ad { get; set; }
+        //public class Ogretmen
+        //{
+        //    public string Ad { get; set; }
 
-            public Ogretmen(string ad)
-            {
-                Ad = ad;
-            }
-        }
+        //    public Ogretmen(string ad)
+        //    {
+        //        Ad = ad;
+        //    }
+        //}
 
 
         
@@ -237,24 +344,24 @@ ORDER BY
         private void button6_Click(object sender, EventArgs e)
         {
             // Ders, Ogretmen, Sinif ve Saat nesnelerini oluşturalım
-            Ogretmen ogretmen1 = new Ogretmen("Öğretmen 1");
-            Ogretmen ogretmen2 = new Ogretmen("Öğretmen 2");
-            Ogretmen ogretmen3 = new Ogretmen("Öğretmen 3");
+            //Ogretmen ogretmen1 = new Ogretmen("Öğretmen 1");
+            //Ogretmen ogretmen2 = new Ogretmen("Öğretmen 2");
+            //Ogretmen ogretmen3 = new Ogretmen("Öğretmen 3");
 
-            Saat saat1 = new Saat("09:00");
-            Saat saat2 = new Saat("10:00");
-            Saat saat3 = new Saat("11:00");
+            //Saat saat1 = new Saat("09:00");
+            //Saat saat2 = new Saat("10:00");
+            //Saat saat3 = new Saat("11:00");
 
-            Sinif sinif1 = new Sinif("Sınıf A");
-            Sinif sinif2 = new Sinif("Sınıf B");
-            Sinif sinif3 = new Sinif("Sınıf C");
+            //Sinif sinif1 = new Sinif("Sınıf A");
+            //Sinif sinif2 = new Sinif("Sınıf B");
+            //Sinif sinif3 = new Sinif("Sınıf C");
 
 
             // Ders örnekleri oluşturalım ve önceki öğretmen, sınıf ve saat nesnelerini kullanarak bu derslere atayalım
-            Ders ders1 = new Ders("Matematik", ogretmen1, sinif1, saat1);
-            Ders ders2 = new Ders("Fizik", ogretmen2, sinif2, saat2);
-            Ders ders3 = new Ders("Kimya", ogretmen3, sinif3, saat3);
-            Ders ders4 = new Ders("Kimya", ogretmen2, sinif2, saat2);
+            Ders ders1 = new Ders("Matematik", "ogretmen1", "sinif1", "saat1");
+            Ders ders2 = new Ders("Matematik3", "ogretmen1", "sinif1", "saat1");
+            Ders ders3 = new Ders("Matematik4", "ogretmen1", "sinif1", "saat1");
+            Ders ders4 = new Ders("Matematik2", "ogretmen1", "sinif1", "saat1");
 
 
             // Dersleri bir liste içinde toplayalım
@@ -264,7 +371,39 @@ ORDER BY
             dersler.Add(ders3);
             dersler.Add(ders4);
 
-            RenklendirmeAlgoritmasi(dersler);
+            Graph graph= new Graph();
+
+            // Dersleri çizgeye ekleme ve komşuluk ilişkilerini oluşturma
+            foreach (Ders ders in dersler)
+            {
+                Node node = new Node(ders.DersAdi, ders.DersinOgretmeni, ders.DersinSinifi, ders.DersinSaati);
+                graph.AddNode(node);
+
+                foreach (Node otherNode in graph.Nodes)
+                {
+                    // Kendi düğümümüzü kontrol etmiyoruz
+                    if (otherNode != node)
+                    {
+                        // Aynı ders adı, öğretmen, sınıf veya saat özelliklerine sahipse kenar oluştur
+                        if (otherNode.DersAdi == node.DersAdi ||
+                            otherNode.Ogretmen == node.Ogretmen ||
+                            otherNode.Sinif == node.Sinif ||
+                            otherNode.Saat == node.Saat)
+                        {
+                            graph.AddEdge(node, otherNode);
+                        }
+                    }
+                }
+            }
+
+
+
+            CheckConflicts(graph);
+            listBox1.Items.Clear();
+            listBox1.Items.Add(graph.Nodes[0].DersAdi);
+            listBox1.Items.Add(graph.Nodes[1].DersAdi);
+            listBox1.Items.Add(graph.Nodes[2].DersAdi);
+            listBox1.Items.Add(graph.Nodes[3].DersAdi);
         }
     }
 }
